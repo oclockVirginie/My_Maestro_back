88924f92814c4017b32f10ee7a55b8f501f77ef2
oclockVirginie <virginie.lebalch@oclock.io>
2025-10-29 14:17:22 +0100

    modif database.js

--- app/db/database.js
+++ app/db/database.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.PG_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
 
 export default sequelize;
